dd0bc2406e3b79c7468d744c602758a0c5b3ce7b
Nando <fernando.deleon@gmail.com>
2026-03-04 10:32:20 +1100

    Initial Commit

---
+++ myLatest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myLatest
+//
+//  Created by Fernando De Leon on 4/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myLatest/myLatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  myLatestApp.swift
+//  myLatest
+//
+//  Created by Fernando De Leon on 4/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct myLatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
